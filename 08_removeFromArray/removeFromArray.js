65e34247fe46da4802a8743e401a4f8583b26fe3
ivan <ivanlowlh@gmail.com>
2025-08-05 21:20:13 +0800

    Complete 08 exercise

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...removeArgs) {
+    // for (let i = 0; i < array.length; i++){
+    //     let num = array[i]
+    //     if (removeArgs.includes(num)) {
+    //         array.splice(i, 1)
+    //         i--
+    //     }
+    // }
+    // return array
+    return array.filter((num) => !removeArgs.includes(num))
 };
 
 // Do not edit below this line
