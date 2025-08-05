536d98ad8fda26532b0d1b95beedf0f73f99a702
ivan <ivanlowlh@gmail.com>
2025-08-05 21:03:13 +0800

    Complete 07 exercise

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,4 +1,7 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let string_array = string.split("")
+    let string_array_rev = string_array.reverse()
+    return string_array_rev.join("")
 
 };
 
