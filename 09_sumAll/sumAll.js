8ae99e3e3c12e5301b8fb86114ff892b60ae9df5
ivan <ivanlowlh@gmail.com>
2025-08-05 21:36:35 +0800

    Complete 09 exercise

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,4 +1,20 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    if (min < 0 || !Number.isInteger(min)) {
+        return "ERROR"
+    }
+    if (max < 0 || !Number.isInteger(max)) {
+        return "ERROR"
+    }
+    if (min > max) {
+        let temp = max;
+        max = min;
+        min = temp;
+    }
+    let sum = 0;
+    for (let i = min; i <= max; i++) {
+        sum += i;
+    }
+    return sum;
 
 };
 
