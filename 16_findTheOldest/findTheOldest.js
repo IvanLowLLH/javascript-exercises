6b869e131385644b8ab53070f1d729ea2acdca8b
ivan <ivanlowlh@gmail.com>
2025-08-08 20:53:54 +0800

    Complete 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+function getAge(person) {
+    if (Object.hasOwn(person, 'yearOfDeath')) {
+        return person.yearOfDeath - person.yearOfBirth
+    }
+    else {
+        return new Date().getFullYear() - person.yearOfBirth
+    }
+}
+const findTheOldest = function(people_array) {
+    people_array.sort((personA, personB) => getAge(personB) - getAge(personA))
+    return people_array[0]
 };
 
 // Do not edit below this line
