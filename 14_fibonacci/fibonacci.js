8bc4ddfa72df75703031705e1d39ca15c8cd3d1d
ivan <ivanlowlh@gmail.com>
2025-08-08 20:35:11 +0800

    Complete 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,25 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    let num = Number(number)
+    let zero_num = 0
+    let first_num = 1
+    let next_num = 0
+    if (num < 0) {
+        return "OOPS"
+    }
+    if (num == 0) {
+        return zero_num
+    }
+    else if (num == 1) {
+        return first_num
+    }
+    else {
+        for (let i = 2; i <= number; i++) {
+            next_num = zero_num + first_num
+            zero_num = first_num
+            first_num = next_num
+        }
+        return next_num
+    }
 };
 
 // Do not edit below this line
